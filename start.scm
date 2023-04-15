4309aab67210268c3509857f283c9878a9a4bd93
Matthias Gatto <uso.cosmo.ray@gmail.com>
2023-04-15 23:15:42 +0200

    add crapload of comment
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

--- start.scm
+++ start.scm
@@ -30,6 +30,7 @@
 
   (define lof_action
     (lambda (wid eves)
+      ;; if keydown is push
       (if (yevIsGrpDown eves (yeGet wid "down_k")) (move_down wid))
       (if (yevIsGrpDown eves (yeGet wid "up_k")) (move_up wid))
       YEVE_ACTION
@@ -39,10 +40,13 @@
   (define lof_init
     (lambda (wid unues_type)
       (display "low_enforcement_agents Widget Init\n")
-      (ywCanvasNewTextByStr wid 40 20 "score:")
 
+      ;; print score on the top of the screen
+      (ywCanvasNewTextByStr wid 40 20 "score:")
+      ;; a line to separte the score from the road
       (ywCanvasNewHSegment wid 0 60 1000 "rgba: 0 0 0 255")
 
+      ;; show the road
       (ywCanvasNewImgByPath wid 125 64 "road.png")
       (ywCanvasNewImgByPath wid 125 (+ 64 125) "road.png")
       (ywCanvasNewImgByPath wid 125 (+ 64 250) "road.png")
@@ -52,10 +56,12 @@
       (ywCanvasNewImgByPath wid (* 125 2) (+ 64 250) "road.png")
       (ywCanvasNewImgByPath wid (* 125 2) (+ 64 375) "road.png")
 
-      (yePushBack wid (yevCreateGrp Y_DOWN_KEY) "down_k")
-      (yePushBack wid (yevCreateGrp Y_UP_KEY) "up_k")
+      ;; create a "group" that match keyboard down/s key
+      (yePushBack wid (yevCreateGrp Y_DOWN_KEY Y_S_KEY) "down_k")
+      ;; create a "group" that match keyboard UP/w key
+      (yePushBack wid (yevCreateGrp Y_UP_KEY Y_W_KEY) "up_k")
 
-
+      ;; add the cop, using yGenericNewCanvasLayer, push it to wid
       (yePushBack  wid
        (yGenericNewCanvasLayer
 	wid
@@ -67,17 +73,27 @@
 	(yaeString "layer-canvas" (yeCreateArray) "type")
 	)
        "guy_layer")
+      ;; action callback called at each tick
       (yeCreateFunction "lof_action" wid "action")
+      ;; set to real time
       (ywSetTurnLengthOverwrite -1)
+      ;; canvas widget, and set a white background
+      ;; yaeString is like yeCreateString, but yeCreateString return the string,
+      ;; and yae, it's parent
       (ywidNewWidget (yaeString "rgba: 255 255 255 255" wid "background") "canvas")
       )
     )
 
+  ;; entry point
   (define mod_init
     (lambda (mod)
       (begin
 	(display "low_enforcement_agents INIT \n\n")
+	;; add smart_cobject, so we can use smart_cobject functions (yGeneric*)
 	(ygAddModule Y_MOD_YIRL mod "smart_cobject")
+	;; ygInitWidgetModule simplify the creation of a mod that contain only a widget
+	;; it also add a starting widget of that widget
+	;; if you want to do that manially, you can do it like it's done in pong
 	(ygInitWidgetModule mod "low_enforcement_agents" (yeCreateFunction "lof_init"))
 	mod
 	)
